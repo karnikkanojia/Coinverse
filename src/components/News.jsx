d33cb71b71eae010b03a2ecffa7bfebc78bce3eb
Karnik Kanojia <72961346+karnikkanojia@users.noreply.github.com>
2022-02-11 15:32:37 +0530

    News section completed with search functionality

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Select, Typography, Col, Row, Avatar, Card } from "antd";
 import moment from "moment";
 
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 const { Text, Title } = Typography;
 const { Option } = Select;
 
@@ -10,10 +11,30 @@ const demo = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
 
 const News = ({ simplified }) => {
 
-    const { data: cryptoNews } = useGetCryptoNewsQuery({newsCategory: 'Cryptocurrency', count: simplified ? 6:12});
-    if(!cryptoNews) return 'Loading...'
+    const [ newsCategory, setNewsCategory ] = useState('Cryptocurrency');
+    const { data } = useGetCryptosQuery(100);
+    const { data: cryptoNews } = useGetCryptoNewsQuery({newsCategory: newsCategory, count: simplified ? 6:12});
+
+
+    if(!cryptoNews?.value) return 'Loading...'
     return (
         <Row gutter={[24, 24]}>
+
+            { !simplified && (
+                <Col span={24}>
+                    <Select
+                        showSearch
+                        className="select-news"
+                        placeholder="Select a Crypto"
+                        optionFilterProp="children"
+                        onChange={(value) => setNewsCategory(value)}
+                        filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                    >
+                        <Option value="Cryptocurrency">Cryptocurrency</Option>
+                        {data?.data?.coins.map((coin) => <Option value={coin.name}>{coin.name}</Option>)}
+                    </Select>
+                </Col>
+            )}
             {cryptoNews.value.map((news, i) => (
                 <Col xs={24} sm={12} lg={8} key={i}>
                     <Card hoverable className="news-card">
@@ -29,8 +50,7 @@ const News = ({ simplified }) => {
                             <div className="provider-container">
                                 <div>
                                     <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demo} />
-                                    <Text className="provider-name">{news.provider[0]?.name}</Text>
-
+                                    <Text className="provider-name">{news.provider[0]?.name }</Text>
                                 </div>
                                 <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
                             </div>
